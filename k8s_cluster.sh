79a34f99bc6d6a13c7bf1f25a037841b0ebf5804
Adriano Canofre <adrianoknofre@gmail.com>
2026-05-13 02:48:47 +0100

    add dependencies

--- k8s_cluster.sh
+++ k8s_cluster.sh
@@ -39,17 +39,59 @@ while [ $# -gt 0 ]; do
 done
 
 
-
+echo "=================================================="
 echo "Checking dependencies..."
+kubectl_version="1.30.0"
 
-for cmd in kubectl docker kind; do
-    if ! command -v "$cmd" >/dev/null 2>&1; then
-        echo "❌ $cmd is not installed or not in PATH"
-        exit 1
-    fi
-done
+version=$(kubectl version --client 2>/dev/null | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n1 | sed 's/v//')
 
-echo "✅ All dependencies are installed"
+if [ -z "$version" ]; then
+    echo "❌ kubectl not found or version not readable"
+    exit 1
+fi
+
+if [ "$(printf '%s\n' "$kubectl_version" "$version" | sort -V | head -n1)" != "$kubectl_version" ]; then
+    echo "❌ kubectl version $version is lower than required $kubectl_version"
+    exit 1
+fi
+
+echo "✅ kubectl version OK ($version >= $kubectl_version)"
+echo "=================================================="
+kind_version="0.20.0"
+
+version=$(kind version 2>/dev/null | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n1 | sed 's/v//')
+
+if [ -z "$version" ]; then
+    echo "❌ kind not installed or version not readable"
+    exit 1
+fi
+
+     
+if [ "$(printf '%s\n' "$kind_version" "$version" | sort -V | head -n1)" != "$kind_version" ]; then
+    echo "❌ kind version $version is lower than required $kind_version"
+    exit 1
+fi
+
+echo "✅ kind version OK ($version >= $kind_version)"
+
+echo "=================================================="
+docker_version="27.0.0"
+
+version=$(docker version --format '{{.Client.Version}}' 2>/dev/null)
+
+if [ -z "$version" ]; then
+    echo "❌ docker not installed or version not readable"
+    exit 1
+fi
+
+if [ "$(printf '%s\n' "$docker_version" "$version" | sort -V | head -n1)" != "$docker_version" ]; then
+    echo "❌ docker version $version is lower than required $docker_version"
+    exit 1
+fi
+
+echo "✅ docker version OK ($version >= $docker_version)"
+
+echo "=================================================="
 
 if [ "$ACTION" = "start" ]; then
 
